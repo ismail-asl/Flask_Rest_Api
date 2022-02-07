aac79bd5aa3ff5774d0f139b7ca90f8cfa1a0a6e
ismailtitas1815 <ismail15-1815@diu.edu.bd>
2022-02-07 13:27:05 +0600

    new commit

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class VideoModel(db.Model):
 
 # db.create_all()
 
-video_put_args = reqparse.RequestParser()
+video_put_args = reqparse.RequestParser()   # requested data recv kore.
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
@@ -28,7 +28,7 @@ video_update_args.add_argument("name", type=str, help="Name of the video is requ
 video_update_args.add_argument("views", type=int, help="Views of the video")
 video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
-resource_fields = {
+resource_fields = {    #it defines how an object should be serialized
 	'id': fields.Integer,
 	'name': fields.String,
 	'views': fields.Integer,
@@ -42,6 +42,14 @@ class Video(Resource):
 		if not result:
 			abort(404, message="Could not find video with that id")
 		return result
+
+	@marshal_with(resource_fields)
+	def post(self):
+		args = video_put_args.parse_args()
+		video = VideoModel(name=args['name'], views=args['views'], likes=args['likes'])
+		db.session.add(video)
+		db.session.commit()
+		return video, 201
 
 	@marshal_with(resource_fields)
 	def put(self, video_id):
--- test.py
+++ test.py
@@ -2,5 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.patch(BASE + "video/2", {})
+#get data
+response = requests.get(BASE + "video/10")
 print(response.json())
+
+#patch or update data
+# response = requests.patch(BASE + "video/1",{"name":"New Name"})
+# print(response.json())
+
+#add or post data
+# response = requests.post(BASE + "video/",{'name': 'post name', 'views': 33000, 'likes': 25000})
+# print(response.json())
